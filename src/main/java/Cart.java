14dccf0f1d8cd189da99b71781474c889a97c21b
stidy <gstudy1021@gmail.com>
2024-05-04 23:10:50 +0900

    카트 작업 중

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -7,20 +7,68 @@ public class Cart extends Item{
 
     //HashMap<String, Item> cartList = new HashMap<>();
 
-    //private Item item;
+   // private Item item;
 
 
+//    @Override
+//    public void Burger(String name, double price, String explain) {
+//        super.Burger(name, price, explain);
+//    }
+//
+//    @Override
+//    public void Drink(String name, double price, String explain) {
+//        super.Drink(name, price, explain);
+//    }
+//
+//    @Override
+//    public void Side(String name, double price, String explain) {
+//        super.Side(name, price, explain);
+//    }
 
-    public List<Cart> Cart() {
+    List<Item> cartBurger = burgerList.stream().toList();
+    List<Item> cartDrink = drinkList.stream().toList();
+    List<Item> cartSide = sideList.stream().toList();
 
-        List<Item> cartItem = new ArrayList<>();
-
-
-        return null;
+    public List<Item> getCartBurger() {
+//        for (int i = 0; i < burgerList.size(); i++) {
+//        }
+        return cartBurger;
     }
+
+
+
+    public List<Item> getCartDrink() {
+        return cartDrink;
+    }
+
+    public List<Item> getCartSide() {
+        return cartSide;
+    }
+
+    public void setCartSide(List<Item> cartSide) {
+        this.cartSide = cartSide;
+    }
+
+    public void setCartBurger(List<Item> cartBurger) {
+        this.cartBurger = cartBurger;
+    }
+
+    public void setCartDrink(List<Item> cartDrink) {
+        this.cartDrink = cartDrink;
+    }
+
+
+
+    //    public void cartList() {
+//        cartItem.add(item.burgerList)
+//        return null;
+//    }
+
 
 //    public void CartList() {
 //        cartList.put("cart", )
 //    }
+
+
 
 }
